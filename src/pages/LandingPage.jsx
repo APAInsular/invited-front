bfc1fe69d3c2919045a5431897a40cbc0a92438c
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-03-12 13:56:44 +0000

    cambio Landfing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -62,17 +62,6 @@ const LandingPage = () => {
 
     return (
         <div className="mt-5" style={{ backgroundColor: "#FAF9F8", color: "#2F2F2F", minHeight: "100vh" }}>
-            {/* Hero Section */}
-            <header className='mt-5' style={{ backgroundColor: "#FAF9F8", color: "white", textAlign: "center", padding: "50px" }}>
-                <h1 className='mt-5' style={{ fontSize: "2.5rem", fontWeight: "bold", color: "#F19292" }}>Crea Invitaciones Personalizadas prueba publica</h1>
-                {user && (
-                    <h3 className='mt-5' style={{ color: "#F19292" }}>Bienvenido {user.name} y {user.partner.name}</h3>
-                )
-                }
-                <p style={{ marginTop: "10px", fontSize: "1.2rem", color: "#F19292" }}>Bodas, cumpleaños y más, con plantillas listas para ti.</p>
-                <Button variant="light" style={{ marginTop: "10px", color: "#FAF9F8", backgroundColor: "#F19292", fontWeight: "bold" }}>Comenzar</Button>
-            </header>
-
             <section id="inicio" class="hero">
                 <div class="hero-overlay">
                     <div class="container hero-content">

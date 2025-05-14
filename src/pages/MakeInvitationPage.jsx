89a3fb2d1709d6f89a9c87866d290f8a77f0b4cf
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-05-14 12:21:11 +0100

    cambio

--- src/pages/MakeInvitationPage.jsx
+++ src/pages/MakeInvitationPage.jsx
@@ -121,7 +121,6 @@ export default function MakeInvitationForm() {
     };
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
         setIsLoading(true);
         const token = sessionStorage.getItem('auth_token');
 

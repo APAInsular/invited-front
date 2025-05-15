0105b556561e42f91ec6c6fb6ccf4da2b3247a0d
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-05-15 10:05:42 +0100

    cambios

--- src/templates/Template_1/components/Gallery_Plantilla_1.jsx
+++ src/templates/Template_1/components/Gallery_Plantilla_1.jsx
@@ -1,33 +1,39 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const Gallery_Plantilla_1 = ({ images, speed }) => {
+const Gallery_Plantilla_1 = ({ images, speed = 20 }) => {
     const sliderRef = useRef(null);
     const [offset, setOffset] = useState(0);
 
     useEffect(() => {
+        const slider = sliderRef.current;
+        const totalWidth = slider.scrollWidth / 2;
+
         const interval = setInterval(() => {
-            setOffset((prevOffset) => prevOffset - 1);
+            setOffset((prevOffset) => {
+                const nextOffset = prevOffset - 1;
+                return Math.abs(nextOffset) >= totalWidth ? 0 : nextOffset;
+            });
         }, speed);
 
         return () => clearInterval(interval);
     }, [speed]);
 
     return (
-        <div class="container">
-            <h2 class="section-title text-center mb-5">Conoce nuestra historia</h2>
-            <div class="row g-4">
+        <div className="container">
+            <h2 className="section-title text-center mb-5">Conoce nuestra historia</h2>
+            <div className="slider-container">
                 <div
                     ref={sliderRef}
                     className="slider-track"
-                    style={{ transform: `translateX(${offset}px)`, transition: "transform 0.1s linear" }}
+                    style={{ transform: `translateX(${offset}px)`, transition: "transform 0.03s linear" }}
                 >
-                    {[...images, ...images, ...images, ...images, ...images, ...images, ...images, ...images].map((image, index) => (
+                    {[...images, ...images].map((image, index) => (
                         <img key={index} src={image} alt={`Slide ${index}`} className="slider-image" />
                     ))}
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default Gallery_Plantilla_1;

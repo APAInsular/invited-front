704dc7fc902c933331f43e0e740834be15e0e97b
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-05-15 10:14:27 +0100

    cambios

--- src/templates/Template_1/components/Gallery_Plantilla_1.jsx
+++ src/templates/Template_1/components/Gallery_Plantilla_1.jsx
@@ -21,13 +21,13 @@ const Gallery_Plantilla_1 = ({ images, speed = 20 }) => {
     return (
         <div className="container">
             <h2 className="section-title text-center mb-5">Conoce nuestra historia</h2>
-            <div className="slider-container">
+            <div className="slider-wrapper">
                 <div
                     ref={sliderRef}
                     className="slider-track"
-                    style={{ transform: `translateX(${offset}px)`, transition: "transform 0.03s linear" }}
+                    style={{ transform: `translateX(${offset}px)`, transition: "transform 0.1s linear" }}
                 >
-                    {[...images, ...images].map((image, index) => (
+                    {[...images, ...images, ...images, ...images].map((image, index) => (
                         <img key={index} src={image} alt={`Slide ${index}`} className="slider-image" />
                     ))}
                 </div>

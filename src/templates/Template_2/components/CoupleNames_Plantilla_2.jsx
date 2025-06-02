f5a8ea6d716b411a82d616bdae006b1c69448544
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-06-02 11:45:42 +0100

    letra disney

--- src/templates/Template_2/components/CoupleNames_Plantilla_2.jsx
+++ src/templates/Template_2/components/CoupleNames_Plantilla_2.jsx
@@ -18,6 +18,17 @@ const CoupleNames_Plantilla_2 = ({ imageUrl, groom, bride, location, date }) => 
             <div className="position-relative d-inline-block mb-4">
                 <img src={imageUrl} alt="Samantha y Javier" className="img-fluid rounded-circle header-photo-template2"></img>
                 <div className="floral-frame-template2"></div>
+                <svg width="0" height="0">
+                    <defs>
+                        <mask id="mascara-corazon" maskContentUnits="objectBoundingBox">
+                            <path fill="white" d="M0.5,0.85 
+                            C0.75,1 1,0.75 1,0.5 
+                            C1,0.25 0.75,0 0.5,0 
+                            C0.25,0 0,0.25 0,0.5 
+                            C0,0.75 0.25,1 0.5,0.85 Z"/>
+                        </mask>
+                    </defs>
+                </svg>
             </div>
             <h1 className="invite-name-template2 mt-3 mb-4">{groom} & {bride}</h1>
             <div className="d-flex justify-content-center">

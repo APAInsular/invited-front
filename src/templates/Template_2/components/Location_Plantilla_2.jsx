c166c293d1d496fa1a4995c52048782fd4112852
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-06-02 13:59:39 +0100

    edicion foto

--- src/templates/Template_2/components/Location_Plantilla_2.jsx
+++ src/templates/Template_2/components/Location_Plantilla_2.jsx
@@ -4,7 +4,7 @@ import { Church } from "lucide-react";
 const Location_Plantilla_2 = ({ location, country }) => {
     return (
         <section className="text-center my-4 py-5">
-            <div className="container mt-4 pt-5">
+            <div className="container mt-4">
                 <h2 className="section-title-template2"><strong>¿A dónde ir?</strong></h2>
                 <Church size={32} className="text-gray-700" />
                 <p className="section-subtitle-template2 mb-0">
